95e79922b0b152973e9d1a05b19e3426af5919ad
Jelena Savenkova <ezhemarked@gmail.com>
2021-10-21 13:56:04 +0300

    Add project files.

---
+++ homework4.3JelenaSavenkova/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace homework4._3JelenaSavenkova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 30, 300, 3000, 30000 };
+            Console.WriteLine(numbers.Sum());
+            Console.ReadLine();
+        }
+    }
+}
